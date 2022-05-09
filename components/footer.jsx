e170d334be58f9c6bab546835f14022b00fc680d
SorinPapa <sorin.papa@ymail.com>
2022-05-09 21:29:47 +0300

    mono chrome setup

---
+++ components/footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <section className="footer-primary">
+
+      </section>
+
+      <section className="footer-secondary text-center">
+      
+      </section>
+    </footer>
+  );
+}
---
+++ components/header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="header container mt-0 mt-lg-4">
+  
+    </header>
+  );
+}
